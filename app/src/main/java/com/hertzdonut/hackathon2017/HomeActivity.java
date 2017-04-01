7e38cab9fee472f187ee153c64945be941c4fe4a
Joe Lodyga <jplodyga@eagle.fgcu.edu>
2017-04-01 16:08:18 -0400

    Login, Register and Home Activities

--- app/src/main/java/com/hertzdonut/hackathon2017/HomeActivity.java
+++ app/src/main/java/com/hertzdonut/hackathon2017/HomeActivity.java
@@ -2,6 +2,7 @@ package com.hertzdonut.hackathon2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,6 +12,21 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
 
+
+    }
+
+    // Method called when Start Reservation button is clicked
+    public void startReservation(View v) {
+
+    }
+
+    // Method called when My Reservation buttons is clicked
+    public void myReservations(View v) {
+
+    }
+
+    // Method called when My Profile button is clicked
+    public void myProfile(View v) {
 
     }
 }
