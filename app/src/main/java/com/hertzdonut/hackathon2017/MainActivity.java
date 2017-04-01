f555deaa53c2bf238a24f71d2fc1c56715dfb6e3
Joe Lodyga <jplodyga@eagle.fgcu.edu>
2017-04-01 14:36:39 -0400

    Initial Project

--- app/src/main/java/com/hertzdonut/hackathon2017/MainActivity.java
+++ app/src/main/java/com/hertzdonut/hackathon2017/MainActivity.java
@@ -1,13 +1,32 @@
 package com.hertzdonut.hackathon2017;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText usernameField, passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameField = (EditText) findViewById(R.id.edtTxtUsername);
+        passwordField = (EditText) findViewById(R.id.edtTxtPassword);
+
+    }
+
+    public void login(View v) {
+        System.out.println("login");
+    }
+
+    public void register(View v) {
+        System.out.println("register");
+        Intent registerIntent = new Intent();
+
     }
 }
